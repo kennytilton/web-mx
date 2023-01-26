791785efc464d58bfb87f28b62c6bdef0cc3ed09
Kenneth Tilton <kentilton@gmail.com>
2023-01-26 11:54:50 -0500

    images

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject com.tiltontec/web-mx "1.0.2-SNAPSHOT"
+(defproject com.tiltontec/web-mx "1.0.0-SNAPSHOT"
   :description "A Web un-Framework powered by Matrix(tm)"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/kennytilton/web-mx/"
   :dependencies [[org.clojure/clojure "1.11.0"]
                  [org.clojure/clojurescript "1.11.4"]
+                 [org.slf4j/slf4j-nop       "1.7.30"]
                  [com.cognitect/transit-cljs "0.8.264"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]
                  [funcool/bide "1.7.0"]

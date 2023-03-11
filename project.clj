13842477dfb36012fe356d8555dec6c1a12c41b2
Kenneth Tilton <kentilton@gmail.com>
2023-03-11 15:18:50 -0500

    Web/MX 2.0.0 Rizing

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.tiltontec/web-mx "1.0.0-SNAPSHOT"
+(defproject com.tiltontec/web-mx "2.0.0-SNAPSHOT"
   :description "A Web un-Framework powered by Matrix(tm)"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -6,13 +6,11 @@
   :dependencies [[org.clojure/clojure "1.11.0"]
                  [org.clojure/clojurescript "1.11.60"]
                  [org.slf4j/slf4j-nop       "1.7.30"]
-                 [com.tiltontec/matrix "4.3.1-SNAPSHOT"]]
+                 [com.tiltontec/matrix "5.0.0-SNAPSHOT"]]
   :profiles
   {:dev
    {:dependencies [[org.clojure/clojurescript "1.10.773"]
-                   [com.bhauman/figwheel-main "0.2.18"]
-                   ;; optional but recommended
-                   #_ [com.bhauman/rebel-readline-cljs "0.1.4"]]
+                   [com.bhauman/figwheel-main "0.2.18"]]
     :resource-paths ["target"]
     :clean-targets ^{:protect false} ["target"]}}
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]}
